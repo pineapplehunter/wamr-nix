dbf2cb64116fd444b216709107c2cb459164d83f
Shogo Takata <peshogo@gmail.com>
2025-07-11 09:21:49 +0900

    organize packages

--- flake.nix
+++ flake.nix
@@ -28,66 +28,63 @@
           directory = ./pkgs;
         };
 
-      packages = eachSystem (pkgs: {
-        default = pkgs.wamr;
-        wamr-classic = pkgs.wamr.override {
-          enable_interp = true;
-          enable_fast_interp = false;
-          enable_aot = false;
-          enable_jit = false;
-          enable_fast_jit = false;
-          enable_lazy_jit = false;
-        };
-        wamr-fast-interp = pkgs.wamr.override {
-          enable_interp = true;
-          enable_fast_interp = true;
-          enable_aot = false;
-          enable_jit = false;
-          enable_fast_jit = false;
-          enable_lazy_jit = false;
-        };
-        wamr-aot = pkgs.wamr.override {
-          enable_interp = false;
-          enable_fast_interp = false;
-          enable_aot = true;
-          enable_jit = false;
-          enable_fast_jit = false;
-          enable_lazy_jit = false;
-        };
-        wamr-jit = pkgs.wamr.override {
-          enable_interp = false;
-          enable_fast_interp = false;
-          enable_aot = false;
-          enable_jit = true;
-          enable_fast_jit = false;
-          enable_lazy_jit = false;
-        };
-        wamr-fast-jit = pkgs.wamr.override {
-          enable_interp = false;
-          enable_fast_interp = false;
-          enable_aot = false;
-          enable_jit = true;
-          enable_fast_jit = true;
-          enable_lazy_jit = false;
-        };
-        wamr-lazy-jit = pkgs.wamr.override {
-          enable_interp = false;
-          enable_fast_interp = false;
-          enable_aot = false;
-          enable_jit = true;
-          enable_fast_jit = false;
-          enable_lazy_jit = true;
-        };
-        wamr-static = pkgs.pkgsStatic.wamr;
-        wamr-static-jit = pkgs.pkgsStatic.wamr.override {
-          enable_interp = false;
-          enable_fast_interp = false;
-          enable_aot = false;
-          enable_jit = true;
-          enable_fast_jit = false;
-          enable_lazy_jit = false;
-        };
-      });
+      packages = eachSystem (
+        pkgs:
+        {
+          default = pkgs.wamr;
+          wamr-classic = pkgs.wamr.override {
+            enable_interp = true;
+            enable_fast_interp = false;
+            enable_aot = false;
+            enable_jit = false;
+            enable_fast_jit = false;
+            enable_lazy_jit = false;
+          };
+          wamr-fast-interp = pkgs.wamr.override {
+            enable_interp = true;
+            enable_fast_interp = true;
+            enable_aot = false;
+            enable_jit = false;
+            enable_fast_jit = false;
+            enable_lazy_jit = false;
+          };
+          wamr-aot = pkgs.wamr.override {
+            enable_interp = false;
+            enable_fast_interp = false;
+            enable_aot = true;
+            enable_jit = false;
+            enable_fast_jit = false;
+            enable_lazy_jit = false;
+          };
+          wamr-jit = pkgs.wamr.override {
+            enable_interp = false;
+            enable_fast_interp = false;
+            enable_aot = false;
+            enable_jit = true;
+            enable_fast_jit = false;
+            enable_lazy_jit = false;
+          };
+          wamr-lazy-jit = pkgs.wamr.override {
+            enable_interp = false;
+            enable_fast_interp = false;
+            enable_aot = false;
+            enable_jit = true;
+            enable_fast_jit = false;
+            enable_lazy_jit = true;
+          };
+          wamr-static = pkgs.pkgsStatic.wamr;
+        }
+        // lib.optionalAttrs (pkgs.hostPlatform.system == "x86_64-linux") {
+          wamr-fast-jit = pkgs.wamr.override {
+            enable_interp = false;
+            enable_fast_interp = false;
+            enable_aot = false;
+            enable_jit = true;
+            enable_fast_jit = true;
+            enable_lazy_jit = false;
+          };
+        }
+      );
 
       checks = eachSystem (pkgs: self.packages.${pkgs.system});
 
